f2cc11c33709ae19c34d7ddc396c08cb35d22c1b
Artem <hartem472@gmail.com>
2024-01-31 18:25:00 +0100

    initialize user and planetarium_api apps

---
+++ planetarium_api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from planetarium_api.models import PlanetariumDome
+
+
+admin.site.register(PlanetariumDome)
---
+++ planetarium_api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
