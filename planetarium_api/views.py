7e2fbdf8ba472e01940fff3e1323462748efefb4
Artem <hartem472@gmail.com>
2024-02-01 09:12:38 +0100

    create views for all models

--- planetarium_api/views.py
+++ planetarium_api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium_api.models import (
+    PlanetariumDome,
+    Reservations,
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
+    Ticket
+)
+from planetarium_api.serializers import (
+    PlanetariumDomeSerializer,
+    ReservationsSerializer,
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    ShowSessionSerializer,
+    TicketSerializer,
+)
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ReservationsViewSet(viewsets.ModelViewSet):
+    queryset = Reservations.objects.all()
+    serializer_class = ReservationsSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
