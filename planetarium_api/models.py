2f8e4541d2cb0f0cc42a7e9139f4173700108341
Artem <hartem472@gmail.com>
2024-02-01 01:01:04 +0100

    cerate all models in planetarium_api

--- planetarium_api/models.py
+++ planetarium_api/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 
+from planetarium_service import settings
+
 
 class PlanetariumDome(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+
+class Reservations(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
+    reservations = models.ForeignKey(Reservations, on_delete=models.CASCADE)
