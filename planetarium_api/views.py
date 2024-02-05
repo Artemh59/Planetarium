478edcb68f7f994f7209a1f262793fd5ff779074
Artem <hartem472@gmail.com>
2024-02-05 15:44:10 +0100

    create own permission class and add permissions for all models

--- planetarium_api/views.py
+++ planetarium_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from planetarium_api.models import (
     PlanetariumDome,
@@ -6,8 +7,9 @@ from planetarium_api.models import (
     ShowTheme,
     AstronomyShow,
     ShowSession,
-    Ticket
+    Ticket,
 )
+from planetarium_api.permissions import IsAdminOrIfAuthenticatedReadOnly
 from planetarium_api.serializers import (
     PlanetariumDomeSerializer,
     ReservationSerializer,
@@ -21,28 +23,40 @@ from planetarium_api.serializers import (
 class PlanetariumDomeViewSet(viewsets.ModelViewSet):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumDomeSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.all()
     serializer_class = AstronomyShowSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.all()
     serializer_class = ShowSessionSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
