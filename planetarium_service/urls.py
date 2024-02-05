ad0616494d9b9276dd2c60fe6cda81a8121b1ff5
Artem <hartem472@gmail.com>
2024-02-05 10:07:14 +0100

    Create user model and register api

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium_api.urls", namespace="planetarium_api")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
