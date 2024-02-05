61bd0d44aa1610c12a4cfbeba4d210adf9d96ceb
Artem <hartem472@gmail.com>
2024-02-05 15:17:11 +0100

    craete 'me' api

--- planetarium_api/models.py
+++ planetarium_api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-from planetarium_service import settings
+from django.contrib.auth import get_user_model
 
 
 class PlanetariumDome(models.Model):
@@ -14,7 +13,7 @@ class PlanetariumDome(models.Model):
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} {self.created_at}"
