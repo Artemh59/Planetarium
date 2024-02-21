d5adf4e8460a9699a6fd505e1e8efe0ed125e0f0
Artem <hartem472@gmail.com>
2024-02-21 15:40:11 +0100

    delete secret key from settings

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^itv_=d!=9sau^i%x$v((59f#8r0xdl@2889sh3g!96uyl8s_x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
